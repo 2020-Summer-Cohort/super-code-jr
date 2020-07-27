062940e80d697799c253d6a468197e2bfd0e16ac
Ben Williams <DeanPBR@gmail.com>
2020-07-27 11:40:24 -0400

    Initial commit

---
+++ src/main/java/co/supercodejr/supercodejr/SupercodejrApplication.java
@@ -0,0 +1,13 @@
+package co.supercodejr.supercodejr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupercodejrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupercodejrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/supercodejr/supercodejr/SupercodejrApplicationTests.java
@@ -0,0 +1,13 @@
+package co.supercodejr.supercodejr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupercodejrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
