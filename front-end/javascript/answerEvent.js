6b8e27ee6d9cc798e67c345a4a64ef5e957d3be3
MilesInSpace <Cshank.public@gmail.com>
2020-07-29 14:38:27 -0400

    created interactive prototype

--- front-end/javascript/answerEvent.js
+++ front-end/javascript/answerEvent.js
@@ -1,23 +1,9 @@
-import { renderContent } from "./codeRender.js";
+import { renderContent, populateCode } from "./codeRender.js";
 const renderDisplay = document.querySelector(".iframe-display");
 
 const answer1 = document.querySelector(".interactive-answer1");
 answer1.addEventListener("click", () => {
   alert("you found bad code!");
   renderContent();
-});
-const answer2 = document.querySelector(".interactive-answer2");
-answer2.addEventListener("click", () => {
-  alert("you found bad code!");
-  renderContent();
-});
-const answer3 = document.querySelector(".interactive-answer3");
-answer3.addEventListener("click", () => {
-  alert("you found bad code!");
-  renderContent();
-});
-const answer4 = document.querySelector(".interactive-answer4");
-answer4.addEventListener("click", () => {
-  alert("you found bad code!");
-  renderContent();
+  populateCode();
 });
