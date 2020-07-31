04dfe6b7e40e6b3690012eb7d0cc7ebc1e9f72fd
Sarah Greene <sgreene807@gmail.com>
2020-07-31 11:02:35 -0400

    contents of files can be read in alphabetical order

--- src/main/java/co/supercodejr/supercodejr/Parser.java
+++ src/main/java/co/supercodejr/supercodejr/Parser.java
@@ -4,22 +4,35 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.lang.reflect.Array;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Parser {
 
-    public static void main(String[] args) throws IOException{
-        String fileName = "C:\\Users\\green\\Documents\\WE Can Code IT\\Code\\super-code-jr\\src\\main\\resources\\test\\test.txt";
-//        ClassLoader classLoader = ClassLoader.getSystemClassLoader();
-        File file = new File(fileName);
-        System.out.println(file.exists());
-        String content = new String(Files.readAllBytes(file.toPath()));
-        System.out.println(content);
-         String[] testArray = content.split("\\.");
-        System.out.println(testArray.length);
-        for (String a : testArray)
-            System.out.println(a);
+    public static void main(String[] args) throws IOException {
+        String fileName = "src/main/resources/test/";
+        File dir = new File(fileName);
+        File[] dirFiles = dir.listFiles();
+        Arrays.sort(dirFiles);
+        for (File file : dirFiles) {
+            System.out.println(file.exists());
+            String content = new String(Files.readAllBytes(file.toPath()));
+//            Files.list(Paths.get(String.valueOf(dir))).sorted().forEach(System.out::println);
+            System.out.println(content);
+        }
     }
 }
+
+//        System.out.println(file.exists());
+//        String content = new String(Files.readAllBytes(file.toPath()));
+//        System.out.println(content);
+//         String[] testArray = content.split("\\.");
+//        System.out.println(testArray.length);
+//        for (String a : testArray)
+//            System.out.println(a);
+//    }
+
 
 
 
