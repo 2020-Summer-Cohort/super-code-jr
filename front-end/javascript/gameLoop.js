cd72167c12b6b42af188b04b7c3db60bbf334d64
MilesInSpace <Cshank.public@gmail.com>
2020-08-03 16:42:04 -0400

    fixed the iframe

--- front-end/javascript/gameLoop.js
+++ front-end/javascript/gameLoop.js
@@ -1,23 +1,16 @@
-import { renderBeforeError, renderErrorCode, renderAfterError, renderCorrectCodeBlock, renderWrongIframeContent, 
-    renderCorrectIFrameContent, renderCodeyDialogue, renderGroundControlBeginning, renderGroundControlFinished, 
-    renderGroundControlHint}from "./codeRender.js"
-import { errorInteractivity} from "./answerEvent.js"
-import { fetchQuestion, fetchQuestions} from "./fetchQuestions.js"
+import { renderAllContent } from "./codeRender.js";
+import { fetchQuestion, fetchQuestions } from "./fetchQuestions.js";
+export { questionGrabber };
+let turnCounter = -1;
 
-let turnCounter = 1
+const questionGrabber = () => {
+  turnCounter++;
+  fetchQuestions().then((question) => {
+    renderAllContent(question[turnCounter], turnCounter);
+  });
+};
+const startGame = () => {
+  questionGrabber(turnCounter);
+};
 
-const questionGrabber = (turnCounter) => {
-    fetchQuestion(turnCounter)
-      .then(question => {
-        renderWrongIframeContent()
-        renderBeforeError(question)
-        renderErrorCode(question)
-        renderAfterError(question)
-        renderCodeyDialogue(question)
-        renderGroundControlBeginning(question)
-    })
-}
-
-questionGrabber(turnCounter)
-
-errorInteractivity()
+startGame(turnCounter);
