8a52dd824c394bdb6eb2b36ae85c7bd1a2b3ca0d
JGibson37 <Jason.Gibson15@yahoo.com>
2020-08-10 15:06:35 -0400

    updated dev display box

--- front-end/javascript/devRender.js
+++ front-end/javascript/devRender.js
@@ -40,41 +40,41 @@ createEventListeners()
 const renderJason = () =>{
 const selectDevBox = document.querySelector(".developer-display")
 selectDevBox.innerHTML= `            
+<container class="developer-img-container">
 <img class="developer-img" src="images/jason.png">
+</container>
 <section class="developer-info-box">
     <a class="bio-anchors" href="http://jgibson37.github.io">Jason Gibson's Portfolio</a>
     <a class="bio-anchors" href="http://www.linkedin.com/in/jgibson37">Jason Gibson's LinkedIn</a>
     <p class="bio-info">Strengths:</p>
     <ul>
-      <li><span class="skill-span"> Java: <div class="skill-bar-1">0000000000000000000</div> </span></li>
-      <li><span class="skill-span"> Html: <div class="skill-bar-2">000000000000000000000000000000</div></span></li>
-      <li><span class="skill-span"> CSS: <div class="skill-bar-3">000000000000000000000000000000</div></span></li>
-      <li><span class="skill-span"> Javascript: <div class="skill-bar-4">000000000000000000</div></span></li>
+      <li><span class="skill-span"> Java: <div class="skill-bar-1">0000000000000</div> </span></li>
+      <li><span class="skill-span"> Html: <div class="skill-bar-2">000000000000</div></span></li>
+      <li><span class="skill-span"> CSS: <div class="skill-bar-3">00000000000</div></span></li>
+      <li><span class="skill-span"> JS: <div class="skill-bar-4">00000000000000</div></span></li>
     </ul>
-    <p class="bio-info">Weakness: dank memes</p>
+    <p class="bio-info">Weakness: Dank memes</p>
     <p class="bio-info">
-        Jason grew up in Columbus, OH. From an early age he was interested in video games and
-        robots. This lead him in highschool to join the FIRST Robotics team and learn about Mechanical Engineering
-        and computer programming. After pursuing Mechanical Engineering and not liking it as much, he's returned to
-        the tech side and has started to pursue his other passion, Software Engineering. He still maintains the Engineering
-        creed, "I am a trained problem solver."
+        Gamer, Memer, laid backer, Go with the flower.
     </p>`
 }
 const renderMiles = () =>{
     const selectDevBox = document.querySelector(".developer-display")
     selectDevBox.innerHTML= `            
+    <container class="developer-img-container">
     <img class="developer-img" src="images/miles.png">
+    </container>
     <section class="developer-info-box">
         <a class="bio-anchors" href="http://jgibson37.github.io">Miles Shank's Portfolio</a>
         <a class="bio-anchors" href="http://www.linkedin.com/in/jgibson37">Miles Shank's LinkedIn</a>
         <p class="bio-info">Strengths:</p>
         <ul>
-          <li><span class="skill-span"> Java: <div class="skill-bar-1">0000000000000000000</div> </span></li>
-          <li><span class="skill-span"> Html: <div class="skill-bar-2">000000000000000000000000000000</div></span></li>
-          <li><span class="skill-span"> CSS: <div class="skill-bar-3">000000000000000000000000000000</div></span></li>
-          <li><span class="skill-span"> Javascript: <div class="skill-bar-4">000000000000000000</div></span></li>
+          <li><span class="skill-span"> Java: <div class="skill-bar-1">000000000000</div> </span></li>
+          <li><span class="skill-span"> Html: <div class="skill-bar-2">00000000000</div></span></li>
+          <li><span class="skill-span"> CSS: <div class="skill-bar-3">00000000000</div></span></li>
+          <li><span class="skill-span"> JS: <div class="skill-bar-4">000000000000000</div></span></li>
         </ul>
-        <p class="bio-info">Weakness: dank memes</p>
+        <p class="bio-info">Weakness: Spiced Peaches</p>
         <p class="bio-info">
             Artist, Developer, Cosplayer, Creator, ask me about Sonic the Hedgehog.
         </p>`
@@ -82,19 +82,21 @@ const renderMiles = () =>{
 
 const renderKenny = () =>{
     const selectDevBox = document.querySelector(".developer-display")
-    selectDevBox.innerHTML= `            
+    selectDevBox.innerHTML= `
+    <container class="developer-img-container">            
     <img class="developer-img" src="images/kw.png">
+    </container>
     <section class="developer-info-box">
         <a class="bio-anchors" href="http://jgibson37.github.io">Kenny Williams's Portfolio</a>
-        <a class="bio-anchors" href="http://www.linkedin.com/in/jgibson37">Kenny Williams's LinkedIn</a>
+        <a class="bio-anchors" href="http://www.linkedin.com/in/kenny-williams-c">Kenny Williams's LinkedIn</a>
         <p class="bio-info">Strengths:</p>
         <ul>
-          <li><span class="skill-span"> Java: <div class="skill-bar-1">0000000000000000000</div> </span></li>
-          <li><span class="skill-span"> Html: <div class="skill-bar-2">000000000000000000000000000000</div></span></li>
-          <li><span class="skill-span"> CSS: <div class="skill-bar-3">000000000000000000000000000000</div></span></li>
-          <li><span class="skill-span"> Javascript: <div class="skill-bar-4">000000000000000000</div></span></li>
+          <li><span class="skill-span"> ui/ux: <div class="skill-bar-1">0000000000000</div> </span></li>
+          <li><span class="skill-span"> Html: <div class="skill-bar-2">00000000000</div></span></li>
+          <li><span class="skill-span"> CSS: <div class="skill-bar-3">0000000000000</div></span></li>
+          <li><span class="skill-span"> JS: <div class="skill-bar-4">0000000000000</div></span></li>
         </ul>
-        <p class="bio-info">Weakness: dank memes</p>
+        <p class="bio-info">Weakness: Sneakers and Fashion</p>
         <p class="bio-info">
             Creative, Frontend Formulator, Master Juicer & Smoothie Maker
         </p>`
@@ -103,18 +105,20 @@ const renderKenny = () =>{
 const renderTrey = () =>{
     const selectDevBox = document.querySelector(".developer-display")
     selectDevBox.innerHTML= `            
+    <container class="developer-img-container">
     <img class="developer-img" src="images/trey.png">
+    </container>
     <section class="developer-info-box">
-        <a class="bio-anchors" href="http://jgibson37.github.io">Trey Grace's Portfolio</a>
-        <a class="bio-anchors" href="http://www.linkedin.com/in/jgibson37">Trey Grace's LinkedIn</a>
+        <a class="bio-anchors" href="http://joetreygrace.github.io">Trey Grace's Portfolio</a>
+        <a class="bio-anchors" href="http://www.linkedin.com/in/trey-grace">Trey Grace's LinkedIn</a>
         <p class="bio-info">Strengths:</p>
-        <ul>
-          <li><span class="skill-span"> Java: <div class="skill-bar-1">0000000000000000000</div> </span></li>
-          <li><span class="skill-span"> Html: <div class="skill-bar-2">000000000000000000000000000000</div></span></li>
-          <li><span class="skill-span"> CSS: <div class="skill-bar-3">000000000000000000000000000000</div></span></li>
-          <li><span class="skill-span"> Javascript: <div class="skill-bar-4">000000000000000000</div></span></li>
+        <ul class="skill-ul">
+          <li><span class="skill-span"> Java: <div class="skill-bar-1">0000000000000</div> </span></li>
+          <li><span class="skill-span"> Html: <div class="skill-bar-2">00000000000</div></span></li>
+          <li><span class="skill-span"> CSS: <div class="skill-bar-3">00000000000</div></span></li>
+          <li><span class="skill-span"> JS: <div class="skill-bar-4">000000000000</div></span></li>
         </ul>
-        <p class="bio-info">Weakness: dank memes</p>
+        <p class="bio-info">Weakness: Krispy Kreme Donuts</p>
         <p class="bio-info">
             API Specialist, Dad Joke Aficionado, Supreme Leader Smoke... Meats
         </p>`
@@ -122,19 +126,21 @@ const renderTrey = () =>{
 
 const renderSarah = () =>{
     const selectDevBox = document.querySelector(".developer-display")
-    selectDevBox.innerHTML= `            
+    selectDevBox.innerHTML= `  
+    <container class="developer-img-container">          
     <img class="developer-img" src="images/sarah.png">
+    </container>
     <section class="developer-info-box">
-        <a class="bio-anchors" href="http://jgibson37.github.io">Sarah Greene's Portfolio</a>
-        <a class="bio-anchors" href="http://www.linkedin.com/in/jgibson37">Sarah Greene's LinkedIn</a>
+        <a class="bio-anchors" href="http://sgreene807.github.io">Sarah Greene's Portfolio</a>
+        <a class="bio-anchors" href="http://www.linkedin.com/in/greene-sarah">Sarah Greene's LinkedIn</a>
         <p class="bio-info">Strengths:</p>
         <ul>
-          <li><span class="skill-span"> Java: <div class="skill-bar-1">0000000000000000000</div> </span></li>
-          <li><span class="skill-span"> Html: <div class="skill-bar-2">000000000000000000000000000000</div></span></li>
-          <li><span class="skill-span"> CSS: <div class="skill-bar-3">000000000000000000000000000000</div></span></li>
-          <li><span class="skill-span"> Javascript: <div class="skill-bar-4">000000000000000000</div></span></li>
+          <li><span class="skill-span"> Java: <div class="skill-bar-1">0000000000000</div> </span></li>
+          <li><span class="skill-span"> Html: <div class="skill-bar-2">000000000000</div></span></li>
+          <li><span class="skill-span"> CSS: <div class="skill-bar-3">00000000000</div></span></li>
+          <li><span class="skill-span"> JS: <div class="skill-bar-4">000000000000</div></span></li>
         </ul>
-        <p class="bio-info">Weakness: dank memes</p>
+        <p class="bio-info">Weakness: Graeter's Ice Cream</p>
         <p class="bio-info">
             Developer, Explorer, Inception Specialist, Doolittle has nothing on me ;-)
         </p>`
@@ -142,20 +148,22 @@ const renderSarah = () =>{
 
 const renderJd = () =>{
     const selectDevBox = document.querySelector(".developer-display")
-    selectDevBox.innerHTML= `            
+    selectDevBox.innerHTML= `           
+    <container class="developer-img-container"> 
     <img class="developer-img" src="images/jd.png">
+    </container>
     <section class="developer-info-box">
-        <a class="bio-anchors" href="http://jgibson37.github.io">JD King's Portfolio</a>
-        <a class="bio-anchors" href="http://www.linkedin.com/in/jgibson37">JD King's LinkedIn</a>
+        <a class="bio-anchors" href="http://patchesohoolihan.github.io">JD King's Portfolio</a>
+        <a class="bio-anchors" href="http://www.linkedin.com/in/gale-king">JD King's LinkedIn</a>
         <p class="bio-info">Strengths:</p>
         <ul>
-          <li><span class="skill-span"> Java: <div class="skill-bar-1">0000000000000000000</div> </span></li>
-          <li><span class="skill-span"> Html: <div class="skill-bar-2">000000000000000000000000000000</div></span></li>
-          <li><span class="skill-span"> CSS: <div class="skill-bar-3">000000000000000000000000000000</div></span></li>
-          <li><span class="skill-span"> Javascript: <div class="skill-bar-4">000000000000000000</div></span></li>
+          <li><span class="skill-span"> Java: <div class="skill-bar-1">0000000000000</div> </span></li>
+          <li><span class="skill-span"> Html: <div class="skill-bar-2">0000000000</div></span></li>
+          <li><span class="skill-span"> CSS: <div class="skill-bar-3">000000000000</div></span></li>
+          <li><span class="skill-span"> JS: <div class="skill-bar-4">000000000000</div></span></li>
         </ul>
-        <p class="bio-info">Weakness: dank memes</p>
+        <p class="bio-info">Weakness: Bees! Not the Bees!</p>
         <p class="bio-info">
-            Back-End Developer, LOTR Savant,<br> Dr. Grant to your Jurassic Park.
+            Back-End Developer, LOTR Savant, Dr. Grant to your Jurassic Park.
         </p>`
 }
